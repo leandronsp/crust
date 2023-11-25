ff26ce66fc87a14f05da21adbaa7bea9d37008e5
leandronsp <leandronsp@gmail.com>
2023-11-24 22:59:24 -0300

    there's no JWT

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
     collections::HashMap,
 };
 
+#[derive(Debug)]
 struct Request {
     verb: String,
     path: String,
@@ -45,32 +46,81 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("{}", "\n\n\n");
-    http_request.iter().for_each(|line| println!("{}", line));
-
     let request = parse_request(http_request);
-    assert_eq!(request.verb, "GET");
-    assert_eq!(request.path, "/");
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
-    let length = contents.len();
+    println!("{}", "\n\n\n");
+    println!("{:#?}", request);
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\nContent-Type: text/html\r\n\r\n{contents}");
-
+    let response = handle_request(request);
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn handle_index(_request: Request) -> String {
+    let mut response = String::new();
+
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    response.push_str("HTTP/1.1 200 OK\r\n");
+    response.push_str("Content-Type: text/html\r\n");
+    response.push_str("\r\n");
+    response.push_str(&contents);
+
+    response
+}
+
+fn handle_404(_request: Request) -> String {
+    let mut response = String::new();
+
+    let contents = fs::read_to_string("404.html").unwrap();
+
+    response.push_str("HTTP/1.1 404 Not Found\r\n");
+    response.push_str("Content-Type: text/html\r\n");
+    response.push_str("\r\n");
+    response.push_str(&contents);
+
+    response
+}
+
+fn handle_request(request: Request) -> String {
+    match (request.verb.as_str(), request.path.as_str()) {
+        ("GET", "/") => handle_index(request),
+        _ =>            handle_404(request),
+    }
 }
 
 fn parse_request(http_request: Vec<String>) -> Request {
     let mut request = Request::new();
     let mut iterator = http_request.iter();
-
     let mut headline_split = iterator.next().unwrap().split(' ');
-    let (verb, path) = (headline_split.next().unwrap(), headline_split.next().unwrap());
+
+    let (verb, path) = (headline_split.next().unwrap(), 
+                        headline_split.next().unwrap());
+
+    println!("{} {}", verb, path);
     
-    request.verb = String::from(verb);
-    request.path = String::from(path);
+    while let Some(line) = iterator.next() {
+        println!("{}", line);
+
+        if line.is_empty() {
+            break;
+        }
+
+        let (key, value) = line.split_once(": ").unwrap();
+
+        if key == "Cookie" {
+            let cookies: Vec<_> = value.split("; ").collect();
+
+            for cookie in cookies {
+                let (key, value) = cookie.split_once("=").unwrap();
+                request.cookies.insert(key.to_string(), value.to_string());
+            }
+        } else {
+            request.headers.insert(key.to_string(), value.to_string());
+        }
+    }
+    
+    request.verb = verb.to_string();
+    request.path = path.to_string();
 
     request
 }
